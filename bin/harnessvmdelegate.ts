2e7ce21170c390cdbebee79be6f875f587fdf808
Martin Ansong <martin.ansong@harness.io>
2023-04-06 10:32:48 +0100

    Add description

--- bin/harnessvmdelegate.ts
+++ bin/harnessvmdelegate.ts
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
+import 'source-map-support/register';
 import { HarnessvmdelegateStack } from '../lib/harnessvmdelegate-stack';
 
 const app = new cdk.App();
 new HarnessvmdelegateStack(app, 'HarnessvmdelegateStack', {
+  description: 'Harness VM Delegate Stack'
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
