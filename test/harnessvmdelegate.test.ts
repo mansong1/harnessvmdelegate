64e5b4823ef8294814e10c72a2de8702aba3914d
Martin Ansong <martin.ansong@harness.io>
2023-04-06 10:34:44 +0100

    Add test for stack

--- test/harnessvmdelegate.test.ts
+++ test/harnessvmdelegate.test.ts
@@ -1,17 +1,24 @@
+import { expect as expectCDK, haveResourceLike } from '@aws-cdk/assert';
+import * as cdk from '@aws-cdk/core';
 // import * as cdk from 'aws-cdk-lib';
 // import { Template } from 'aws-cdk-lib/assertions';
-// import * as Harnessvmdelegate from '../lib/harnessvmdelegate-stack';
+import * as Harnessvmdelegate from '../lib/harnessvmdelegate-stack';
 
 // example test. To run these tests, uncomment this file along with the
 // example resource in lib/harnessvmdelegate-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new Harnessvmdelegate.HarnessvmdelegateStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+test('Check InstanceType', () => {
+    const app = new cdk.App();
+    const stack = new Harnessvmdelegate.HarnessvmdelegateStack(app, 'MyTestStack');
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+    expectCDK(stack).to(haveResourceLike('AWS::EC2::Instance', {
+        InstanceType: 't2.large'
+    }));
+    //     // WHEN
+    //   const stack = new Harnessvmdelegate.HarnessvmdelegateStack(app, 'MyTestStack');
+    //     // THEN
+    //   const template = Template.fromStack(stack);
+
+    //   template.hasResourceProperties('AWS::SQS::Queue', {
+    //     VisibilityTimeout: 300
+    //   });
 });
