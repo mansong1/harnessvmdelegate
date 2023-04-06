44cf2f2c034bb5b00132d0c2f24966dca01e8a56
Martin Ansong <martin.ansong@harness.io>
2023-04-06 11:41:43 +0100

    Add jest test

--- test/harnessvmdelegate.test.ts
+++ test/harnessvmdelegate.test.ts
@@ -1,24 +1,17 @@
-import { expect as expectCDK, haveResourceLike } from '@aws-cdk/assert';
-import * as cdk from '@aws-cdk/core';
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
+import * as cdk from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
 import * as Harnessvmdelegate from '../lib/harnessvmdelegate-stack';
 
 // example test. To run these tests, uncomment this file along with the
 // example resource in lib/harnessvmdelegate-stack.ts
 test('Check InstanceType', () => {
+    // GIVEN
     const app = new cdk.App();
+    // WHEN
     const stack = new Harnessvmdelegate.HarnessvmdelegateStack(app, 'MyTestStack');
-
-    expectCDK(stack).to(haveResourceLike('AWS::EC2::Instance', {
+    const template = Template.fromStack(stack);
+    // THEN
+    template.hasResourceProperties('AWS::EC2::Instance', {
         InstanceType: 't2.large'
-    }));
-    //     // WHEN
-    //   const stack = new Harnessvmdelegate.HarnessvmdelegateStack(app, 'MyTestStack');
-    //     // THEN
-    //   const template = Template.fromStack(stack);
-
-    //   template.hasResourceProperties('AWS::SQS::Queue', {
-    //     VisibilityTimeout: 300
-    //   });
+    })
 });
