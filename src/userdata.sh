8d29cfb7814f47b261ea11210a812f6e37a5aac6
Martin Ansong <martin.ansong@harness.io>
2023-04-06 14:37:47 +0100

    Add commands to start docker-compose after file is added

--- src/userdata.sh
+++ src/userdata.sh
@@ -1,16 +1,20 @@
 #!/bin/bash -xe
 
+# Install Docker
 sudo su
 yum -y update
-sudo yum -y install docker git
+sudo yum -y install docker
 sudo systemctl enable docker.service
 sudo systemctl start docker.service
 sudo usermod -a -G docker ec2-user
 docker info
 
+# Install Docker Compose
 sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 export PATH=$PATH:/usr/local/bin/docker-compose
 docker-compose version
 
-mkdir -p /runner
+# Run the Docker Compose command
+cd /runner
+docker-compose -f docker-compose.yml up -d
